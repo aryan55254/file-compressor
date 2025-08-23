750f1f67fdb7053faff4b87ff2b19f9d75a0b404
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-08-23 12:51:56 +0530

    built huffman tree and generated compression codes

--- main.cpp
+++ main.cpp
@@ -21,6 +21,21 @@ struct CompareNodes
     }
 };
 
+void generate_codes(HuffmanNode *node, std::string current_code, std::map<char, std::string> &codes_map)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    if (node->left == nullptr && node->right == nullptr)
+    {
+        codes_map[node->data] = current_code;
+        return;
+    }
+    generate_codes(node->left, current_code + "0", codes_map);
+    generate_codes(node->right, current_code + "1", codes_map);
+};
+
 int main()
 {
 
@@ -48,6 +63,23 @@ int main()
         HuffmanNode *pnode = new HuffmanNode{pair.first, pair.second, nullptr, nullptr};
         pq.push(pnode);
     };
+    while (pq.size() > 1)
+    {
+        HuffmanNode *left_child = pq.top();
+        pq.pop();
+        HuffmanNode *right_child = pq.top();
+        pq.pop();
 
+        HuffmanNode *parent = new HuffmanNode{'$', left_child->frequency + right_child->frequency, left_child, right_child};
+
+        pq.push(parent);
+    };
+    HuffmanNode *root = pq.top();
+    std::map<char, std::string> huffman_codes;
+    generate_codes(root, "", huffman_codes);
+    for (const auto &pair : huffman_codes)
+    {
+        std::cout << "character : " << pair.first << " huffman_code : " << pair.second << std::endl;
+    }
     return 0;
 }
